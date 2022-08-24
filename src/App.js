0dbaa59e413322c7f680d751212f8af975f62da3
jineu <a11718trannhuy@gmail.com>
2022-08-24 11:33:32 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import picture from './assets/IMG_1048.JPG'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHouseChimney, faCircleUser, faEnvelopeOpen, faMessage, faBriefcase } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+        <div className='container'>
+            <img alt='profile-pic' src={picture} className='profilePic' />
+            <div className='info'>
+                <h3>hi there !</h3>
+                <h1>i'm a <span>web designer</span></h1>
+                <h6>
+                    I'm a Tusinia based web designer & front-end developer
+                    focused on crafting 
+                    <br/>
+                    clean & user-friendly experiences.I'm passionate about building exellent
+                    <br/>
+                    software that improves the lives of those around me.
+                </h6>
+                <input type={'button'} value={'MORE ABOUT ME'} className={'moreBtn'} />
+            </div>
+            <div className='sidebar'>
+                <div className='homeBtn'>
+                    <input type={'button'} value={'HOME'}  className={'home'}/>
+                    <FontAwesomeIcon icon={faHouseChimney} fontSize={'25px'}/>
+                </div>
+                <div className='icons'>
+                    <div className='user'>
+                        <FontAwesomeIcon icon={faCircleUser} fontSize={'25px'}/>
+                    </div>
+                    <div className='briefcase'>
+                        <FontAwesomeIcon icon={faBriefcase} fontSize={'25px'}/>
+                    </div>
+                    <div className='envelope'>
+                        <FontAwesomeIcon icon={faEnvelopeOpen} fontSize={'25px'}/>
+                    </div>
+                    <div className='message'>
+                        <FontAwesomeIcon icon={faMessage} fontSize={'25px'}/>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
   );
 }
